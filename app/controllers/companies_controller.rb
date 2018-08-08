2307fe66668530e1717d71a6e0099e3b545ffb1b
Iana Markevitch <iana.markevitch@gmail.com>
2018-08-07 22:58:05 -0700

    Add home page, navbar & company create form

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,21 @@
 class CompaniesController < ApplicationController
+
+    def new
+        @company = Company.new
+        @industry = Industry.all
+        @subindustry = Subindustry.all 
+        @region = Region.all 
+        @subregion = Subregion.all
+    end
+
+    def create
+        puts company_params
+        @company = Company.new company_params
+        render json: @company
+    end
+
+    private
+    def company_params
+        params.require(:company).permit(:name, :region_id, :subregion_id, :industry_id, :subindustry_id)
+    end
 end
